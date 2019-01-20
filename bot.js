a3e9811a4b1463c764952ff635ffa3ee425177d0
waleed91919 <40673746+waleed91919@users.noreply.github.com>
2019-01-20 14:13:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -564,6 +564,27 @@ client.on('message', message=> {
     }
 });
 
+client.on("message", (message) => {
+                        if (message.content.startsWith("$cv")) {
+                                    if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+                                let args = message.content.split(" ").slice(1);
+                            message.guild.createChannel(args.join(' '), 'voice');
+                            message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+                            
+                        }
+                        });
+
+client.on("message", (message) => {
+                        if (message.content.startsWith("$ct")) {
+                                    if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+                                let args = message.content.split(" ").slice(1);
+                            message.guild.createChannel(args.join(' '), 'text');
+                            message.channel.sendMessage('تـم إنـشاء روم كتابي')
+                            
+                        }
+                        });
+
+
 
 
 client.login(process.env.BOT_TOKEN);
