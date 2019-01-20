7cd166be0cad5ba55e9b5e77b5a7066cc5fb5f4f
waleed91919 <40673746+waleed91919@users.noreply.github.com>
2019-01-20 14:14:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -563,6 +563,7 @@ client.on('message', message=> {
     message.reply(`**My Prefix is **$`)
     }
 });
+//CHANNELS
 
 client.on("message", (message) => {
                         if (message.content.startsWith("$cv")) {
@@ -583,6 +584,34 @@ client.on("message", (message) => {
                             
                         }
                         });
+
+client.on('message', message => {
+    var prefix = "$";
+     if(message.content === prefix + "mutechannel") {
+     if(!message.channel.guild) return message.reply('** This command only for servers**');
+                    
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+         message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+                    
+    }).then(() => {
+             message.reply("**__تم تقفيل الشات__ ✅ **")
+       });
+    }
+    if(message.content === prefix + "unmutechannel") {
+    if(!message.channel.guild) return message.reply('** This command only for servers**');
+                    
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+        message.channel.overwritePermissions(message.guild.id, {
+        SEND_MESSAGES: true
+                    
+         }).then(() => {
+          message.reply("**__تم فتح الشات__✅**")
+      });
+  }
+                           
+});
+
 
 
 
