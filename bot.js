9e760d6a3c7167fb18bb8cd022dbb8e48438035f
waleed91919 <40673746+waleed91919@users.noreply.github.com>
2019-01-20 14:35:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,24 @@ const moment = require('moment');
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+//owner
+client.on('message', async message => {
+if(message.content.startsWith(prefix + "adminbc")) {
+  let i = client.users.size;
+  if(message.author.id !== '386859869052862466') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+  var args = message.content.split(' ').slice(1).join(' ');
+  if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+  setTimeout(() => {
+    message.channel.send(`تم الارسال لـ ${i} شخص`)
+  }, client.users.size * 500);
+  client.users.forEach(s => {
+    s.send(args).catch(e => i--);
+  });
+}
+});
+
+
+
 
 
    client.on("message", message => {
