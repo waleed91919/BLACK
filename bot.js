b961754c4d33a198a8c32e530a5d03d6250721c6
waleed91919 <40673746+waleed91919@users.noreply.github.com>
2019-01-20 13:45:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,6 +58,31 @@ client.on("message", message => {
         }
     });
 
+//topinv
+client.on('message' , async (message) => {
+var prefix = "%"
+    if(message.content.startsWith(prefix + "topinv")) {
+if(message.author.bot) return;
+if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
+  var invites = await message.guild.fetchInvites();
+    invites = invites.array();
+    arraySort(invites, 'uses', { reverse: true });
+    let possibleInvites = ['User Invited |  Uses '];
+    invites.forEach(i => {
+        if (i.uses === 0) { 
+            return;
+        }
+      possibleInvites.push(['\n\ ' +'<@'+ i.inviter.id +'>' + '  :  ' +   i.uses]);
+     //معلومه بسيطه يمديك تكرر العمليهه أكثر من مره
+    })
+    const embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+    .addField("Top Invites." ,`${(possibleInvites)}`)
+
+    message.channel.send(embed)
+    }
+});
+
 
 
 
